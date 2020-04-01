9ba320208a823fb48de70eb1b6d7de0031c1b5a9
alexey-rise <alexey.konovalov@risenext.ca>
2020-04-01 11:29:54 -0400

    fix linting errors

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
-chrome.runtime.onInstalled.addListener(function(details) {
-   	analytics.track("Install", {reason: details.reason, version: chrome.app.getDetails().version});
+chrome.runtime.onInstalled.addListener(details=>{
+  analytics.track("Install", {reason: details.reason, version: chrome.app.getDetails().version});
 });
 
-chrome.runtime.onStartup.addListener(function() {
-	analytics.track("Startup", {version: chrome.app.getDetails().version});
+chrome.runtime.onStartup.addListener(()=>{
+  analytics.track("Startup", {version: chrome.app.getDetails().version});
 });
